c168874b926626c147d684cf41d9ad5499e8bcd6
Peter <peterteach@gmail.com>
2017-02-01 17:26:44 -0800

    Initial code commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public Hydrogen hydrogenPrefab;
+    public List<Hydrogen> hydrogens = new List<Hydrogen>();
+
+    // Use this for initialization
+    void Start () {
+        hydrogens.Add(Instantiate(
+            hydrogenPrefab,
+            new Vector3(Random.Range(-9.0f, 9.0f), Random.Range(-9.0f, 9.0f), Random.Range(-9.0f, 9.0f)),
+            Quaternion.identity));
+        hydrogens.Add(Instantiate(
+            hydrogenPrefab,
+            new Vector3(Random.Range(-9.0f, 9.0f), Random.Range(-9.0f, 9.0f), Random.Range(-9.0f, 9.0f)),
+            Quaternion.identity));
+        hydrogens.Add(Instantiate(
+            hydrogenPrefab,
+            new Vector3(Random.Range(-9.0f, 9.0f), Random.Range(-9.0f, 9.0f), Random.Range(-9.0f, 9.0f)),
+            Quaternion.identity));
+        hydrogens.Add(Instantiate(
+            hydrogenPrefab,
+            new Vector3(Random.Range(-9.0f, 9.0f), Random.Range(-9.0f, 9.0f), Random.Range(-9.0f, 9.0f)),
+            Quaternion.identity));
+        hydrogens.Add(Instantiate(
+            hydrogenPrefab,
+            new Vector3(Random.Range(-9.0f, 9.0f), Random.Range(-9.0f, 9.0f), Random.Range(-9.0f, 9.0f)),
+            Quaternion.identity));
+        hydrogens.Add(Instantiate(
+            hydrogenPrefab,
+            new Vector3(Random.Range(-9.0f, 9.0f), Random.Range(-9.0f, 9.0f), Random.Range(-9.0f, 9.0f)),
+            Quaternion.identity));
+        hydrogens.Add(Instantiate(
+            hydrogenPrefab,
+            new Vector3(Random.Range(-9.0f, 9.0f), Random.Range(-9.0f, 9.0f), Random.Range(-9.0f, 9.0f)),
+            Quaternion.identity));
+        hydrogens.Add(Instantiate(
+            hydrogenPrefab,
+            new Vector3(Random.Range(-9.0f, 9.0f), Random.Range(-9.0f, 9.0f), Random.Range(-9.0f, 9.0f)),
+            Quaternion.identity));
+        hydrogens.Add(Instantiate(
+            hydrogenPrefab,
+            new Vector3(Random.Range(-9.0f, 9.0f), Random.Range(-9.0f, 9.0f), Random.Range(-9.0f, 9.0f)),
+            Quaternion.identity));
+        hydrogens.Add(Instantiate(
+            hydrogenPrefab,
+            new Vector3(Random.Range(-9.0f, 9.0f), Random.Range(-9.0f, 9.0f), Random.Range(-9.0f, 9.0f)),
+            Quaternion.identity));
+        foreach (Hydrogen hydrogen in hydrogens)
+        {
+            hydrogen.GetComponent<Rigidbody>().velocity = new Vector3(
+                Random.Range(-5.0f, 5.0f),
+                Random.Range(-5.0f, 5.0f),
+                Random.Range(-5.0f, 5.0f));
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Hydrogen.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hydrogen : MonoBehaviour {
+
+    AudioSource audioSource;
+    public AudioClip audioClipBallBounce;
+
+    GameObject partner = null;
+    SpringJoint springJoint;
+
+    Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (partner == null && other.gameObject.CompareTag("Hydrogen"))
+        {
+            Hydrogen otherHydrogen = (Hydrogen)other.gameObject.GetComponent("Hydrogen");
+
+            if (otherHydrogen.partner == null) // two free radicals meet and form covalent bond
+            {
+                partner = other.gameObject;
+                otherHydrogen.partner = this.gameObject;
+
+                // chemical bond formation suddenly pulls slightly closer together
+                float deltaX = partner.transform.position.x - this.transform.position.x;
+                float deltaY = partner.transform.position.y - this.transform.position.y;
+                float deltaZ = partner.transform.position.z - this.transform.position.z;
+                this.transform.position = new Vector3(
+                    this.transform.position.x + 0.25f * deltaX,
+                    this.transform.position.y + 0.25f * deltaY,
+                    this.transform.position.z + 0.25f * deltaZ);
+                partner.transform.position = new Vector3(
+                    partner.transform.position.x - 0.25f * deltaX,
+                    partner.transform.position.y - 0.25f * deltaY,
+                    partner.transform.position.z - 0.25f * deltaZ);
+
+                // create SpringJoint to implement covalent bond between these two atoms
+                springJoint = this.gameObject.AddComponent<SpringJoint>();
+                springJoint.connectedBody = other.gameObject.GetComponent<Rigidbody>();
+                springJoint.anchor = new Vector3(0, 0, 0);
+                springJoint.connectedAnchor = new Vector3(0, 0, 0);
+                springJoint.spring = 10;
+                springJoint.minDistance = 0.0f;
+                springJoint.maxDistance = 0.0f;
+                springJoint.tolerance = 0.025f;
+                springJoint.breakForce = Mathf.Infinity;
+                springJoint.breakTorque = Mathf.Infinity;
+                springJoint.enableCollision = false;
+                springJoint.enablePreprocessing = true;
+            }
+        }
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            audioSource.PlayOneShot(audioClipBallBounce, 0.25f);
+        }
+    }
+
+}
